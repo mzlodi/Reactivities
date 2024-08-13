4690201648b857585eb37d3cbef3fe559fddb0e2
Marija Zlodi <marija.zlodi01@gmail.com>
2024-08-13 22:14:40 +0200

    End of section 17

--- client-app/src/app/stores/userStore.ts
+++ client-app/src/app/stores/userStore.ts
@@ -45,4 +45,8 @@ export default class UserStore {
             console.log(error);
         }
     }
+
+    setImage = (image: string) => {
+        if (this.user) this.user.image = image;
+    }
 }
